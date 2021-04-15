4bf32c7929b79ebe02fac7338cd9732b89a4b62d
JohnBox <johnboxcodes@gmail.com>
2021-04-14 23:59:57 -0300

    include icons and fix alignment

--- src/components/bottom-bar/bottom-bar.tsx
+++ src/components/bottom-bar/bottom-bar.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const home = require("../../assets/images/home.svg") as string;
+const libraries = require("../../assets/images/book.svg") as string;
+const profile = require("../../assets/images/profile.svg") as string;
+
+
 const BottomBarTag = styled.div`
   position: absolute;
   bottom: 20px;
@@ -9,17 +14,37 @@ const BottomBarTag = styled.div`
   right: 20px;
   height: 40px;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 `;
+const Label = styled.span`
+  display: block;
+  font-size: 10px;
+`;
+
+const BarItem = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 function BottomBar() {
   return (
     <div>
       <BottomBarTag>
-      <Link to="/">Home </Link>
-      <Link to="/detail">Detail </Link>
-      <Link to="/search">Search </Link>
+      <Link to="/">
+      <BarItem>
+        <img height={16} src={home} />  
+        <Label>Home</Label>
+      </BarItem>  
+      </Link>
+      <BarItem>
+        <img height={16} src={libraries} />  
+        <Label>Libraries</Label>
+      </BarItem>  
+      <BarItem>
+        <img height={16} src={profile} />  
+        <Label>Profile</Label>
+      </BarItem>  
       </BottomBarTag>
     </div>
   )
