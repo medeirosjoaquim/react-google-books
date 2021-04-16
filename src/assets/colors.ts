1f1261942d47b2f7b4fa978d63481b8c99e8b6eb
JohnBox <johnboxcodes@gmail.com>
2021-04-16 00:17:47 -0300

    basic styles for input

--- src/assets/colors.ts
+++ src/assets/colors.ts
@@ -1,5 +1,6 @@
 export const colors = {
   appDark:  '#3f4043',
+  appDark50: '#54565a',
   appDarkBlue: '#00173d',
   appGray:  '#bfbebf',
   appCream: '#f2f2f2',
--- src/components/search-input/search-input.tsx
+++ src/components/search-input/search-input.tsx
@@ -1,6 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
+import { colors } from '../../assets/colors';
 
+//TODO  add icon magnifier 
+const StyledInput = styled.input`
+	border: 1px solid black;
+	border-radius: 3px;
+	padding: 0 8px;
+	box-sizing: border-box;
+  font-size: 16px;
+  //text-align: center;
+  line-height: 18px;
+  width: 336px;
+  height: 48px;
+  left: 19px;
+  top: 50px;
+  
+  background: #FDFCFC;
+  box-shadow: 5px 5px 80px rgba(212, 173, 134, 0.122623);
+  border-radius: 10px;
+	&::placeholder {
+		color: ${colors.appDark50};
+	}
+	&:focus {
+		background-color: #fafafa;
+		border: none;
+    outline: none;
+    // dont remove outline for accessibility
+    box-shadow: 0 0 1px 2px ${colors.appDark};
+	}
+`;
 const SearchInputContainer = styled.div`
   top: 20px;
   left: 20px;
@@ -26,7 +55,7 @@ border-radius: 10px;
 function SearchInput() {
   return (
     <SearchInputContainer>
-      <input type="text"/>
+      <StyledInput/>
     </SearchInputContainer>
   )
 }
